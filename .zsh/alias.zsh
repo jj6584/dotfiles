9ddb8508c94da0d991d2852403014c2116fda08c
Joshua Manaol <joshua.manaol@gmail.com>
2023-07-08 08:49:02 +0800

    added config for wsl

---
+++ .zsh/alias.zsh
@@ -0,0 +1,10 @@
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias python='python3'
+
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
+alias wopen='explorer.exe .'
+alias k='kubectl'
+
